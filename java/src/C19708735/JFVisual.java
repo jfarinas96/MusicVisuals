47da6e70191b5a69859191cecf58bebd943ce000
Janae Fariñas <C19708735@mytudublin.ie>
2021-03-31 15:42:39 +0100

    start

---
+++ java/src/C19708735/JFVisual.java
@@ -0,0 +1,54 @@
+package C19708735;
+
+import ie.tudublin.Visual;
+
+public class JFVisual extends Visual {
+
+    int scene = 0;
+
+    public void keyPressed()
+    {
+        if (keyCode == ' ')
+        {
+            getAudioPlayer().cue(0);
+            getAudioPlayer().play();
+        }
+
+        if (keyCode >= '0' && keyCode <= '3') {
+            scene = keyCode - '0';
+        }
+    }
+
+    public void settings() {
+        size(1024, 500);
+    }
+
+    public void setup() {
+        startMinim();
+        loadAudio("crash-my-car.mp3");
+    }
+
+    public void draw() {
+        background(0);
+
+        switch(scene) {
+            case 0: {
+
+                break;
+            }
+
+            case 1: {
+                break;
+            }
+            
+            case 2: {
+                break;
+            }
+            
+            case 3: {
+                break;
+            }
+        }
+        
+    }
+}
--- java/src/example/RotatingAudioBands.java
+++ java/src/example/RotatingAudioBands.java
@@ -32,7 +32,7 @@ public class RotatingAudioBands extends Visual {
         setFrameSize(256);
 
         startMinim();
-        loadAudio("heroplanet.mp3");
+        loadAudio("crash-my-car.mp3");
         getAudioPlayer().play();
         //startListening(); 
         
